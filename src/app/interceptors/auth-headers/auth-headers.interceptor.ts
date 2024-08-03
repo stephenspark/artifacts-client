7d309929e4d2caa595f645b8c4d135729c28e52b
Stephen Park <ssp0929@gmail.com>
2024-08-03 13:14:31 -0700

    chore: Update auth-headers.interceptor.ts to use TokenService for API token

--- src/app/interceptors/auth-headers/auth-headers.interceptor.ts
+++ src/app/interceptors/auth-headers/auth-headers.interceptor.ts
@@ -1,12 +1,16 @@
+import { inject } from '@angular/core';
 import { HttpContextToken, HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { TokenService } from '../../services';
 
 export const SERVICE_GROUP = new HttpContextToken(() => 'default');
 
 export const authHeadersInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenService = inject(TokenService);
+
   if (req.context.get(SERVICE_GROUP) === 'artifacts') {
     const apiUrl = environment.apiUrl;
-    const apiToken = environment.artifactsApiToken;
+    const apiToken = tokenService.token;
     req = req.clone({
       url: `${apiUrl}` + req.url.replace('http://localhost:4200', ''),
       headers: req.headers
@@ -14,8 +18,6 @@ export const authHeadersInterceptor: HttpInterceptorFn = (req, next) => {
         .set('Accept', 'application/json')
         .set('Content-Type', 'application/json'),
     });
-
-    console.log(req);
   }
 
   return next(req);
