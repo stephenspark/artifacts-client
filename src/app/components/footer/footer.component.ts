3f0a991c5b7d7fafd51c19070ee7e9e439d7964c
Stephen Park <ssp0929@gmail.com>
2024-08-03 13:14:19 -0700

    chore: Update footer component styles and template

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, Subject, interval } from 'rxjs';
+import { startWith, switchMap, tap } from 'rxjs/operators';
+
+import { ArtifactsStatusService, Status } from '../../services';
+
+import packageJson from '../../../../package.json';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  appVersion: string = packageJson.version;
+  angularVersion: string = packageJson.dependencies['@angular/common'].replace(
+    '^',
+    '',
+  );
 
+  status$: Observable<Status>;
+  status: Signal<Status | undefined>;
+  statusLoading: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private artifactsStatusService: ArtifactsStatusService) {
+    // Set up polling every 10 seconds
+    this.status$ = interval(10000).pipe(
+      startWith(0),
+      tap(() => this.statusLoading.next(true)),
+      switchMap(() => this.artifactsStatusService.getStatus()),
+      tap(() => this.statusLoading.next(false)),
+    );
+
+    // Convert the polling observable to a signal
+    this.status = toSignal(this.status$);
+  }
 }
